807f5e83382f4cc23f9041d5aa96b61f0d550c86
BananaWanted <i@BananaWanted.com>
2018-07-27 13:57:37 +0800

    update indent

--- applications/db-vcs/sqitch/deploy/001_create_table_for_ddns.sql
+++ applications/db-vcs/sqitch/deploy/001_create_table_for_ddns.sql
@@ -13,10 +13,9 @@ $$ LANGUAGE plpgsql;
 
 
 CREATE TABLE user_ident (
-    id      BIGSERIAL,
-    email   VARCHAR(255)    NOT NULL,
-    name    VARCHAR(255)    NOT NULL,
-    created_time TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
+    id                      BIGSERIAL                   NOT NULL,
+    email                   VARCHAR(255)                NOT NULL,
+    created_time            TIMESTAMP WITHOUT TIME ZONE NOT NULL    DEFAULT now(),
 
     PRIMARY KEY (id),
     UNIQUE (email)
@@ -24,11 +23,10 @@ CREATE TABLE user_ident (
 
 
 CREATE TABLE ddns_record (
-    user_id     BIGINT          NOT NULL,
-    secret_id   VARCHAR(255)    NOT NULL DEFAULT gen_random_id_for_human(),
-    public_id   VARCHAR(255)    NOT NULL DEFAULT gen_random_id_for_human(),
-    created_time TIMESTAMP WITHOUT TIME ZONE
-                                NOT NULL DEFAULT now() ,
+    user_id                 BIGINT                      NOT NULL,
+    secret_id               VARCHAR(255)                NOT NULL    DEFAULT gen_random_id_for_human(),
+    public_id               VARCHAR(255)                NOT NULL    DEFAULT gen_random_id_for_human(),
+    created_time            TIMESTAMP WITHOUT TIME ZONE NOT NULL    DEFAULT now(),
 
     PRIMARY KEY (secret_id),
     FOREIGN KEY (user_id) REFERENCES user_ident(id) ON DELETE RESTRICT ON UPDATE CASCADE,
@@ -38,12 +36,11 @@ CREATE TABLE ddns_record (
 
 
 CREATE TABLE ddns_remote_report (
-    id          BIGSERIAL,
-    user_id     BIGINT          NOT NULL,
-    secret_id   VARCHAR(255)    NOT NULL,
-    ip          INET            NOT NULL,
-    created_time TIMESTAMP WITHOUT TIME ZONE
-                                NOT NULL DEFAULT now(),
+    id                      BIGSERIAL                   NOT NULL,
+    user_id                 BIGINT                      NOT NULL,
+    secret_id               VARCHAR(255)                NOT NULL,
+    ip                      INET                        NOT NULL,
+    created_time            TIMESTAMP WITHOUT TIME ZONE NOT NULL    DEFAULT now(),
 
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES user_ident(id) ON DELETE RESTRICT ON UPDATE CASCADE,
