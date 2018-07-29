f76b3b3294039a9a6e728d8e405af8fc95c00cee
BananaWanted <i@BananaWanted.com>
2018-07-29 16:49:18 +0800

    move to GINO

--- applications/dns/ddns/ddns_setup.py
+++ applications/dns/ddns/ddns_setup.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python3
+from textwrap import dedent
 
 from sanic.request import Request
 from sanic.response import text
 from sanic.views import HTTPMethodView
 
-from ddns.orm import DDNSRecord
-from utils.db import enable_session
+from orm import DDNSRecord, db
 
 
 class DDNSSetupView(HTTPMethodView):
-
-    decorators = [enable_session]
-
     async def get(self, request: Request, user_id: int):
-        record = DDNSRecord(user_id=user_id)
-        request.app.session.add(record)
-        await request.app.session_flush()
+        async with db.transaction():
+            record = await DDNSRecord(user_id=user_id).create()
 
-        report_url = request.app.url_for(
-            'ddns.DDNSReportView',
-            secret_id=record.secret_id,
-            _external=True,
-            _scheme=request.scheme,
-            _server=request.host,
-        )
-        cname_host = "{}.{}".format(record.public_id, request.app.config.DDNS_ZONE)
+            report_url = request.app.url_for(
+                'ddns.DDNSReportView',
+                secret_id=record.secret_id,
+                _external=True,
+                _scheme=request.scheme,
+                _server=request.host,
+            )
+            cname_host = "{}.{}".format(record.public_id, request.app.config.DDNS_ZONE)
 
-        return text("{}\n{}".format(report_url, cname_host))
+            return text(dedent(f"""\
+                report to {report_url}
+                set cname target to {cname_host}
+                """))
---
+++ applications/dns/orm.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import uuid
+from datetime import datetime
+
+from gino.ext.sanic import Gino
+
+db = Gino()
+
+
+class DDNSRecord(db.Model):
+    __tablename__ = 'ddns_record'
+
+    user_id = db.Column(db.BIGINT, db.ForeignKey("user_identifiers.id"))
+    created_time = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_time = db.Column(db.DateTime, default=datetime.utcnow)
+
+    secret_id = db.Column(db.VARCHAR, default=lambda: uuid.uuid4().hex, primary_key=True)
+    public_id = db.Column(db.VARCHAR, default=lambda: uuid.uuid4().hex)
+
+
+class DDNSRemoteReport(db.Model):
+    __tablename__ = 'ddns_remote_report'
+
+    user_id = db.Column(db.BIGINT, db.ForeignKey("user_identifiers.id"))
+    created_time = db.Column(db.DateTime, default=datetime.utcnow)
+
+    id = db.Column(db.BIGINT, primary_key=True)
+    secret_id = db.Column(db.VARCHAR, db.ForeignKey('ddns_record.secret_id'))
+    ip = db.Column(db.INET)
