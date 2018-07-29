b0159c760d9b1a734fd94bd06200865b09563c2d
BananaWanted <i@BananaWanted.com>
2018-07-29 15:46:13 +0800

    update service definition

--- applications/auth/app.py
+++ applications/auth/app.py
@@ -4,6 +4,7 @@ from sanic import Sanic
 from sanic.request import Request
 
 from models import db
+from views import AuthDescribe, AuthGetToken, AuthCreateRole
 
 app = Sanic()
 
@@ -13,14 +14,12 @@ if app.config.get('DB_USERNAME'):
     app.config.DB_DATABASE = app.config.DB_USERNAME
 db.init_app(app)
 
-#
-# [app.add_route(handler, path) for handler, path in [
-#     (),
-#     (),
-#     (),
-#     (),
-# ]]
-#
+
+[app.add_route(handler, path) for handler, path in [
+    (AuthDescribe.as_view(), "/auth/describe/<identifier_type>/<identifier>"),  # describe identifier (user_id / email / token)
+    (AuthGetToken.as_view(), "/auth/get_token/"),     # verify identity and produce an access token
+    (AuthCreateRole.as_view(), "/auth/create_role/"),    # create user
+]]
 
 
 if __name__ == "__main__":
--- applications/auth/models.py
+++ applications/auth/models.py
@@ -3,8 +3,11 @@
 import enum
 from datetime import datetime
 from enum import auto
+from functools import reduce
+from typing import List
 
 from gino.ext.sanic import Gino
+from sqlalchemy import and_
 
 db = Gino()
 
@@ -20,6 +23,12 @@ class PermissionFlag(enum.Enum):
     DENY = auto()
 
 
+class GrantController:
+    @classmethod
+    async def create_from_context(cls, auth_context: dict):
+        pass
+
+
 class UserIdentifiers(db.Model):
     __tablename__ = "user_identifiers"
 
@@ -29,7 +38,7 @@ class UserIdentifiers(db.Model):
     updated_time = db.Column(db.DateTime, default=datetime.utcnow)
 
 
-class AuthGrantEmailValidate(db.Model):
+class AuthGrantEmailValidate(db.Model, GrantController):
     __tablename__ = "auth_grant_email_validate"
 
     user_id = db.Column(db.BIGINT, db.ForeignKey("user_identifiers.id"), primary_key=True)
@@ -40,7 +49,7 @@ class AuthGrantEmailValidate(db.Model):
     validate_status = db.Column(db.Boolean, default=False)
 
 
-class AuthGrantPassword(db.Model):
+class AuthGrantPassword(db.Model, GrantController):
     __tablename__ = "auth_grant_password"
 
     user_id = db.Column(db.BIGINT, db.ForeignKey("user_identifiers.id"), primary_key=True)
@@ -61,3 +70,47 @@ class AuthPermissionFlags(db.Model):
 
     resource_ddns = db.Column(db.Enum(PermissionFlag))
     resource_server_info = db.Column(db.Enum(PermissionFlag))
+
+
+class User:
+    user_identifiers: UserIdentifiers
+    permission_flags: AuthPermissionFlags
+    grant_controllers: List[GrantController]
+
+    @classmethod
+    async def new_user(cls, email: str, auth_context: dict):
+        self = cls()
+        # TODO: validate email format
+        self.user_identifiers = await UserIdentifiers.create(email=email)
+        self.permission_flags = await AuthPermissionFlags.create(user_id=self.user_identifiers.id)
+
+        auth_context.update({
+            "email": email,
+            "user_id": self.user_identifiers.id,
+        })
+
+        for controller_cls in (AuthGrantEmailValidate, AuthGrantPassword):
+            self.grant_controllers.append(await controller_cls.create_from_context(auth_context))
+
+        return self
+
+    @classmethod
+    async def load_user(cls, user_id=None, email=None):
+        self = cls()
+        conds = []
+        if user_id:
+            conds.append(UserIdentifiers.id == user_id)
+        if email:
+            conds.append(UserIdentifiers.email == email)
+        if not conds:
+            raise ValueError("either user_id or email should be provided to find a user")
+        where = reduce(lambda x, y: and_(x, y), conds[1:], conds[0])
+        self.user_identifiers = await UserIdentifiers.query.where(where).gino.one()
+        self.permission_flags = await AuthPermissionFlags.query.where(AuthPermissionFlags.user_id == self.user_identifiers.id).gino.one()
+        self.grant_controllers = []
+        for controller_cls in (AuthGrantEmailValidate, AuthGrantPassword):
+            self.grant_controllers.append(await controller_cls.query.where(controller_cls.user_id == self.user_identifiers.id).gino.one())
+        return self
+
+    async def update_user(self):
+        pass
