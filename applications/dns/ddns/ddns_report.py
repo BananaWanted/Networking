46161f35e5003393cbf67457663eb009eaf62ceb
BananaWanted <i@BananaWanted.com>
2018-07-29 21:48:56 +0800

    make dns update the zone even in local testing

--- applications/dns/ddns/ddns_report.py
+++ applications/dns/ddns/ddns_report.py
@@ -21,16 +21,12 @@ class DDNSReportView(HTTPMethodView):
                 ip=request.remote_addr).create()
         # release the DB connection, since Google Cloud operations are time consuming.
         endpoint = "{}.{}.".format(record.public_id, request.app.config.DDNS_ZONE)
-        if request.app.config.get("TESTING"):
-            return text(dedent(f"""\
+        await asyncio.get_event_loop().run_in_executor(None, self.update_zone, endpoint, request.remote_addr)
+        return (dedent(f"""\
             set endpoint {endpoint} to ip {request.remote_addr}
-            """))
-        else:
-            # un-blocking the main thread
-            # but the operation is still blocking.
-            await asyncio.get_event_loop().run_in_executor(None, self.update_zone, endpoint, request.remote_addr)
-            return text(endpoint)
 
+            this may take minutes to update
+        """))
 
     @staticmethod
     def update_zone(endpoint, ip):
@@ -40,7 +36,7 @@ class DDNSReportView(HTTPMethodView):
             if endpoint.endswith(zone.dns_name):
                 break
         else:
-            raise NotFound("setup not found")
+            raise NotFound("zone not found")
 
         changes = zone.changes()
 
