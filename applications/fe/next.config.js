ab8bfd0ba3148915afad117de55eaf4f308cbbe0
BananaWanted <i@BananaWanted.com>
2018-08-18 00:56:35 +0800

    update packages, update next config

--- applications/fe/next.config.js
+++ applications/fe/next.config.js
@@ -1,7 +1,10 @@
 module.exports = {
     distDir: 'dist',
-    generateBuildId: async () => {
-    // For example get the latest git commit hash here
-    return 'my-build-id'
-    }
+    generateBuildId: () => process.env.BUILD_TAG,
+    serverRuntimeConfig: {
+    },
+    publicRuntimeConfig: {
+        AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+        AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    },
 }
